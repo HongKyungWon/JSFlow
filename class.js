e1e07fce43c57419fcb5b28509e75216d4c51569
HongKyungWon <hkw7984@naver.com>
2020-07-18 21:16:48 +0900

    second commit

--- class.js
+++ class.js
@@ -1,16 +1,26 @@
-var extendClass = (function(){
-    function Bridge(){}
-    return function(Parent, Child){
-        Bridge.prototype = Parent.prototype;
-        Child.prototype = new Bridge();
-        Child.prototype.constructor = Child;
-        Child.prototype.superClass = Parent;
+class Person {
+    constructor (name, age) {
+        this.name = name || '이름없음';
+        this.age = age || '나이모름';
     }
-})();
-function Person(name, age){
-    this.name = name || '이름없음';
-    this.age = age || '나이모름';
+    getName() {
+        return this.name;
+    }
+    getAge () {
+        return this.age;
+    }
 }
-Person.prototype.getName = function() {
-    return this.name; //ㅓㅣ마퍼ㅏㅇㅍㅇ
+class Employee extends Person {
+    constructor (name, age, position) {
+        super(name, age);
+        this.position = position || '직책모름';
+    }
+    getPosition () {
+        return this.position;
+    }
 }
+const a = Employee;
+a._name = "김세환";
+a.age = 25;
+a.position = '사원';
+console.log(a);
